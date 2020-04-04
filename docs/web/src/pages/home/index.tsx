e7a053c63939eaebb754021591c21b23845c023e
Dax Ravi <d@ironbay.co>
2020-04-04 16:09:32 -0400

    Markdown generator

--- docs/web/src/pages/home/index.tsx
+++ docs/web/src/pages/home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebar from '/structures/sidebar'
+import ReactMarkdown from 'react-markdown'
+
 
 export default function Home() {
     return (
@@ -54,27 +56,18 @@ export default function Home() {
 
                     <div className="min-h-screen w-full lg:static lg:max-h-full lg:overflow-visible lg:w-3/4 xl:w-4/5 p-12">
                         <div >
-                            <Title>Beyond REST, ORMs and Pub/Sub</Title>
-                            <div className="h-6" />
-                            <Paragraph>
-                                Most application frameworks ship with tools to define a REST API for your frontends, an ORM to transform incoming data into something your database understands, and if you're lucky, a pub/sub system to keep everything in sync.
-                                <br />
-                                <br />
-                                Your energy is spent juggling these disparate systems and building the glue that holds them together. Nevermind the tools you're missing to manage your increasingly complex business logic.
-                                <br />
-                                <br />
-                                Riptide eliminates that pain by taking an entirely different approach.  With Riptide you are only responsible for defining
-                                .
-                                <br />
-                                <br />
-                                1. Validate and augment incoming writes and reads
-                                <br />
-                                2. Store and retreive data
-                                <br />
-                                3. Trigger side effects when data changes
-                                <br />
-                                4. Automatically keep all copies of data in sync — whether that's on the server or client
-                            </Paragraph>
+                            <ReactMarkdown
+                                escapeHtml={false}
+                                renderers={{
+                                    heading: props => {
+                                        if (props.level === 1)
+                                            return <h1 className="text-3xl font-200 tracking-wide text-gray-700 mb-6" {...props} />
+                                    },
+                                    paragraph: props => <p className="leading-relaxed text-gray-700 mb-6" {...props} />
+                                }}
+                            >
+                                {require('bundle-text:/markdown/overview.md')}
+                            </ReactMarkdown>
                         </div>
                     </div>
                 </div>
