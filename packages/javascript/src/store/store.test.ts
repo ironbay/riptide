5201680813bd8029a849d52fdfd1ffcd079bd495
Alan Rice <mail@alanrice.me>
2020-04-05 16:24:23 -0400

    fe test

--- packages/javascript/src/store/store.test.ts
+++ packages/javascript/src/store/store.test.ts
@@ -10,47 +10,47 @@ import Memory from './memory'
 
                 await store.mutation({
                     merge: {
-                        animals: {
-                            shark: "hammerhead"
+                        todos: {
+                            todo_01: "hammerhead"
                         }
                     },
                     delete: {}
                 })
                 store.mutation_reverse({
                     merge: {
-                        animals: {
-                            shark: 'great white',
-                            whale: 'orca',
+                        todos: {
+                            todo_01: 'pet the great white',
+                            todo_02: 'feed the orca',
                         }
                     },
                     delete: {}
                 })
-                expect(store.query({})).toEqual({ animals: { shark: "hammerhead" } })
+                expect(store.query({})).toEqual({ todos: { todo_01: "hammerhead" } })
 
                 await store.mutation({
                     delete: {
-                        animals: {
-                            shark: 1
+                        todos: {
+                            todo_01: 1
                         }
                     },
                     merge: {}
                 })
-                expect(store.query({})).toEqual({ animals: {} })
+                expect(store.query({})).toEqual({ todos: {} })
 
                 await store.mutation({
-                    delete: { animals: {} },
-                    merge: { animals: { fish: "barracuda", shark: "hammerhead" } }
+                    delete: { todos: {} },
+                    merge: { todos: { todo_01: "feed the barracuda", todo_02: "pet the hammerhead" } }
                 })
-                expect(store.query({})).toEqual({ animals: { fish: "barracuda", shark: "hammerhead" } })
-                expect(store.query({ animals: { shark: {} } })).toEqual({ animals: { shark: "hammerhead" } })
-                expect(store.query({ animals: { stingray: { climate: {} } } })).toEqual({ animals: { stingray: { climate: undefined } } })
+                expect(store.query({})).toEqual({ todos: { todo_01: "feed the barracuda", todo_02: "pet the hammerhead" } })
+                expect(store.query({ todos: { todo_01: {} } })).toEqual({ todos: { todo_01: "feed the barracuda" } })
+                expect(store.query({ todos: { todo_03: { created: {} } } })).toEqual({ todos: { todo_03: { creaded: undefined } } })
 
-                expect(store.query_path(['animals', 'shark'])).toEqual('hammerhead')
-                expect(store.query_keys(['animals'])).toEqual(['fish', 'shark'])
-                await store.merge(['animals', 'whale'], 'orca')
-                expect(store.query_path(['animals', 'whale'])).toEqual('orca')
-                await store.delete(['animals', 'whale'])
-                expect(store.query_path(['animals', 'whale'])).toEqual(undefined)
+                expect(store.query_path(['todos', 'todo_01'])).toEqual('feed the barracuda')
+                expect(store.query_keys(['todos'])).toEqual(['todo_01', 'todo_02'])
+                await store.merge(['todos', 'todo_01'], 'feed the orca')
+                expect(store.query_path(['todos', 'todo_01'])).toEqual('feed the orca')
+                await store.delete(['todos', 'todo_01'])
+                expect(store.query_path(['todos', 'todo_01'])).toEqual(undefined)
             })
 
         })
