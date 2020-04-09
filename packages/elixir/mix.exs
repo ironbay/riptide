f68d40a5ae32c9b974eb4e2c46c99b5881c95155
Dax Ravi <d@ironbay.co>
2020-04-09 16:35:07 -0400

    Update mix.exs (#8)

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Riptide.MixProject do
   def project do
     [
       app: :riptide,
-      version: "0.2.2",
+      version: "0.2.3",
       description: "Framework for building realtime applications",
       package: [
         maintainers: ["thdxr", "ironbay"],
