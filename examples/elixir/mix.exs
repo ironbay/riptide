fe76e80b75d5305457f40ef0901b51af3c8c7fb7
Dax Ravi <d@ironbay.co>
2020-05-05 14:57:03 -0400

    Make example reference local copy of riptide

--- examples/elixir/mix.exs
+++ examples/elixir/mix.exs
@@ -24,7 +24,8 @@ defmodule Todo.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:riptide, "> 0.0.0", allow_pre: true},
+      # {:riptide, "> 0.0.0", allow_pre: true},
+      {:riptide, path: "../../packages/elixir", allow_pre: true},
       {:bridge_lmdb, "~> 0.1.1"}
     ]
   end
