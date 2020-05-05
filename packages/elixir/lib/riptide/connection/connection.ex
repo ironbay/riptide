8ab5822682e7a359c381a6d5460107e7ec991338
Dax Ravi <d@ironbay.co>
2020-05-05 16:58:55 -0400

    Monitor when calling connection

--- packages/elixir/lib/riptide/connection/connection.ex
+++ packages/elixir/lib/riptide/connection/connection.ex
@@ -1,11 +1,20 @@
 defmodule Riptide.Connection do
   @moduledoc false
   def call(pid, action, body) do
+    ref = Process.monitor(pid)
     send(pid, {:riptide_call, action, body, self()})
 
     receive do
-      {:riptide_response, "error", response} -> {:error, response}
-      {:riptide_response, "reply", response} -> {:ok, response}
+      {:riptide_response, "error", response} ->
+        Process.demonitor(ref)
+        {:error, response}
+
+      {:riptide_response, "reply", response} ->
+        Process.demonitor(ref)
+        {:ok, response}
+
+      {:DOWN, ^ref, _, _, _} ->
+        {:error, :connection_crashed}
     end
   end
 
