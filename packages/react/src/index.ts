4ec174b4be112850f95b27f33658b64dc701d6d4
Alan Rice <mail@alanrice.me>
2020-05-07 11:07:58 -0400

    Cleanup

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,9 +2,9 @@ import * as React from "react"
 import * as Riptide from "@ironbay/riptide"
 
 export function useRiptide(local: Riptide.Store.Memory) {
-  const [num, render] = React.useState(0)
+  const [, render] = React.useState(0)
 
   React.useEffect(() => {
-    local.onChange.add(() => render(num + 1))
-  })
+    local.onChange.add(val => render(val + 1))
+  }, [])
 }
--- packages/react/src/test/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function App() {
-  const [count, set_count] = React.useState(0)
-
-  return <div>{count}</div>
-}
-
-// export default App
