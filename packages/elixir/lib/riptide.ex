7ca4fb0be6b2cbacc68afc318e385d567081b6a1
Dax Ravi <d@ironbay.co>
2020-05-28 14:26:16 -0400

    Documentation for main riptide module

--- packages/elixir/lib/riptide.ex
+++ packages/elixir/lib/riptide.ex
@@ -1,13 +1,27 @@
 defmodule Riptide do
-  @moduledoc false
+  @moduledoc """
+  The primary Riptide module
+  """
+
   @internal %{internal: true}
 
   use Supervisor
 
-  def start_link(opts \\ %{}) do
+  @doc """
+  Starts a Riptide process.
+
+  Probably should not called this directly and instead should be placed
+  inside your application's root supervisor.
+
+  ## Options
+
+  * `:port` - Optional, will override default port of `12_000`
+  """
+  def start_link(opts \\ []) do
     Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
+  @doc false
   def init(opts) do
     Riptide.Store.init()
     Riptide.Migration.run()
@@ -25,6 +39,26 @@ defmodule Riptide do
     )
   end
 
+  @doc """
+  Pass in a query and get the results.
+
+  Read more about query structure [here](https://riptide.ironbay.co/docs/queries). State parameter is optional and is passed to interceptors.
+
+  ## Options
+  * `:min` - Starting range of query, optional
+  * `:max` - End range of query, optional
+  * `:limit` - Max number of results, optional
+
+  ## Examples
+      iex> Riptide.query(%{ "todo:info" => %{} })
+      %{
+        "todo:info" => %{
+          "todo1" => %{
+            "text" => "Document riptide"
+          }
+        }
+      }
+  """
   def query(query, state \\ @internal) do
     with :ok <- Riptide.Interceptor.query_before(query, state) do
       case Riptide.Interceptor.query_resolve(query, state) do
@@ -34,6 +68,20 @@ defmodule Riptide do
     end
   end
 
+  @doc """
+  Return a stream of values underneath a path
+
+  ## Options
+  * `:min` - Starting range of query, optional
+  * `:max` - End range of query, optional
+  * `:limit` - Max number of results, optional
+
+  ## Examples
+      iex> Riptide.stream(["todo:info"]) |> Enum.take(1)
+      [
+        %{"todo1", %{ "text" => "Document riptide" }}
+      ]
+  """
   def stream(path, opts \\ %{}, state \\ @internal) do
     query = Dynamic.put(%{}, path, opts)
 
@@ -42,11 +90,22 @@ defmodule Riptide do
     end
   end
 
+  @doc """
+    The same as `query_path/3` but raises an exception if it fails
+  """
   def query_path!(path, opts \\ %{}, state \\ @internal) do
     {:ok, result} = query_path(path, opts, state)
     result
   end
 
+  @doc """
+  Return data under a specific path
+
+  ## Options
+  * `:min` - Starting range of query, optional
+  * `:max` - End range of query, optional
+  * `:limit` - Max number of results, optional
+  """
   def query_path(path, opts \\ %{}, state \\ @internal) do
     case query(Dynamic.put(%{}, path, opts), state) do
       {:ok, result} -> {:ok, Dynamic.get(result, path)}
@@ -54,21 +113,36 @@ defmodule Riptide do
     end
   end
 
-  def mutation!(mut) do
-    case mutation(mut) do
-      {:ok, result} -> result
-    end
-  end
-
-  def mutation!(mut, state) do
+  @doc """
+    The same as `mutation/2` but raises an exception if it fails
+  """
+  def mutation!(mut, state \\ @internal) do
     case mutation(mut, state) do
       {:ok, result} -> result
     end
   end
 
-  def mutation(mut), do: mutation(mut, %{internal: true})
+  @doc """
+  Apply a mutation.
+  This will do following steps in order
+  1. Trigger `c:Riptide.Interceptor.mutation_before/4`
+  2. Trigger `c:Riptide.Interceptor.mutation_effect/4`
+  3. Broadcast mutation to interested processes
+  4. Write mutation to stores
+  5. Trigger `c:Riptide.Interceptor.mutation_after/4`
 
-  def mutation(mut, state) do
+  ## Examples
+      iex> Riptide.mutation(Riptide.Mutation.merge(["foo", "bar"], "hello"))
+      {:ok, %{
+        merge: %{
+          "foo" => %{
+            "bar" => "hello
+          }
+        },
+        delete: %{}
+      }}
+  """
+  def mutation(mut, state \\ @internal) do
     with {:ok, prepared} <- Riptide.Interceptor.mutation_before(mut, state),
          prepared <- Riptide.Interceptor.mutation_effect(prepared, state),
          :ok <- Riptide.Subscribe.broadcast_mutation(prepared),
@@ -78,29 +152,52 @@ defmodule Riptide do
     end
   end
 
-  def merge(path, value), do: mutation(Riptide.Mutation.merge(path, value))
-  def merge(path, value, state), do: mutation(Riptide.Mutation.merge(path, value), state)
+  @doc """
+  Convience method to apply a mutation that merges a single value
 
-  def merge!(path, value) do
-    {:ok, result} = mutation(Riptide.Mutation.merge(path, value))
+  ## Examples
+      iex> Riptide.merge(["foo", "bar"], "hello")
+      {:ok, %{
+        merge: %{
+          "foo" => %{
+            "bar" => "hello
+          }
+        },
+        delete: %{}
+      }}
+  """
+  def merge(path, value, state \\ @internal),
+    do: mutation(Riptide.Mutation.merge(path, value), state)
+
+  @doc """
+    The same as `merge/3` but raises an exception if it fails
+  """
+  def merge!(path, value, state \\ @internal) do
+    {:ok, result} = merge(path, value, state)
     result
   end
 
-  def merge!(path, value, state) do
-    {:ok, result} = mutation(Riptide.Mutation.merge(path, value), state)
-    result
-  end
+  @doc """
+  Convience method to apply a mutation that deletes a single path
 
-  def delete(path), do: mutation(Riptide.Mutation.delete(path))
-  def delete(path, state), do: mutation(Riptide.Mutation.delete(path), state)
+  ## Examples
+      iex> Riptide.delete(["foo", "bar"])
+      {:ok, %{
+        delete: %{
+          "foo" => %{
+            "bar" => 1
+          }
+        },
+        delete: %{}
+      }}
+  """
+  def delete(path, state \\ @internal), do: mutation(Riptide.Mutation.delete(path), state)
 
-  def delete!(path) do
-    {:ok, result} = mutation(Riptide.Mutation.delete(path))
-    result
-  end
-
-  def delete!(path, state) do
-    {:ok, result} = mutation(Riptide.Mutation.delete(path), state)
+  @doc """
+    The same as `delete/2` but raises an exception if it fails
+  """
+  def delete!(path, state \\ @internal) do
+    {:ok, result} = delete(path, state)
     result
   end
 end
