5b96bd37116844d52ba806943c53bdd437192ff8
Dax Ravi <d@ironbay.co>
2020-10-03 19:48:18 -0400

    Tree concept instead of structure

--- packages/elixir/.formatter.exs
+++ packages/elixir/.formatter.exs
@@ -1,7 +1,9 @@
 # Used by "mix format"
 [
   locals_without_parens: [
-    schema: 1
+    schema: 1,
+    branch: 1,
+    branch: 2
   ],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
